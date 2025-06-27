6d2c110e847cdbf34a7b446121e0ea458a4671a6
sinsang2752 <sinsang2752@naver.com>
2025-06-27 14:35:52 +0900

    Create Programmers_같은 숫자는 싫어_신현상.java

---
+++ 02_스택/Programmers_같은 숫자는 싫어_신현상.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        
+        Stack<Integer> stack = new Stack<>();
+
+        for (int num : arr) {
+            if (stack.isEmpty() || num != stack.peek()) {
+                stack.push(num);
+            }
+        }
+
+        answer = stack.stream().mapToInt(Integer::intValue).toArray();
+
+        return answer;
+    }
+}
