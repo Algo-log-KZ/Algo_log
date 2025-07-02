61bbb300aad105cef875e7345c86b620c93f5ca6
aaa67 <114573522+aaa67@users.noreply.github.com>
2025-07-02 13:03:43 +0900

    Create Programmers_폰켓몬_신정인.py

---
+++ 04_해시/Programmers_폰켓몬_신정인.py
@@ -0,0 +1,19 @@
+def solution(nums):
+    answer = 0
+    data={}
+    check=set()
+    for n in nums:
+        data[n]=data.get(n,0)+1
+    for key in data.keys():
+        if key not in check and len(check)<len(nums)//2:
+            check.add(key)
+            answer+=1
+    return answer
+
+
+def solution(nums):
+    answer = 0
+    n1=len(nums)//2
+    data=list(set(nums))
+    n2=len(data)
+    return min(n1,n2)
