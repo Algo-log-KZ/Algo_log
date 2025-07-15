27886901e5b19511f5beefb0865c66c9103eb94c
Seozzy <77264706+rlatjdus0827@users.noreply.github.com>
2025-07-15 11:25:23 +0900

    Create Programmers_즐겨찾기가 가장 많은 식당 정보 출력하기_김서연.sql

---
+++ 17_SQL/Programmers_즐겨찾기가 가장 많은 식당 정보 출력하기_김서연.sql
@@ -0,0 +1,12 @@
+SELECT FOOD_TYPE
+    , REST_ID
+    , REST_NAME
+    , FAVORITES
+FROM REST_INFO
+WHERE   (food_type, favorites) IN
+            (
+            SELECT food_type, MAX(favorites)
+            FROM rest_info
+            GROUP BY food_type
+            )
+ORDER BY FOOD_TYPE DESC;
