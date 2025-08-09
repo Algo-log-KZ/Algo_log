9bf28d28bdf76227a8ad6e4967add2c07d3dc504
sinsang2752 <sinsang2752@naver.com>
2025-08-09 21:32:05 +0900

    Create Programmers_구명보트_신현상.java

---
+++ 16_그리디/Programmers_구명보트_신현상.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        
+        int minIdx = 0;
+        int maxIdx = people.length - 1;
+        
+        while(minIdx <= maxIdx) {
+            if(people[minIdx] + people[maxIdx] <= limit) {
+                minIdx++;
+            }
+            maxIdx--;
+            answer++;
+        }
+        return answer;
+    }
+}
