607147e8b12182f214432303014ce39a2907562f
김민영 <166481938+minyeong3192@users.noreply.github.com>
2025-08-17 19:54:27 +0900

    Create Programmers_큰 수 만들기_김민영.py

---
+++ 16_그리디/Programmers_큰 수 만들기_김민영.py
@@ -0,0 +1,14 @@
+def solution(number, k):
+    answer = [] # Stack
+    
+    for num in number:
+        while k > 0 and answer and answer[-1] < num:
+            answer.pop()
+            k -= 1
+        answer.append(num)
+        
+    while k > 0:
+        answer.pop()
+        k -= 1
+      
+    return ''.join(answer)
